e74f660d1ec98960a2384c0333c8c3158fd5f141
Kevin Moyer <kevin7.moyer@gmail.com>
2022-10-18 20:09:28 -0500

    readme completed and console logs removed

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 # fetch_backend_exercise
+OVERVIEW
+This service accepts HTTP request for transactions to store, use, and query a user's reward points.
 
-How-To:
-To start the server, from the root folder, run `npm run dev`.
+
+API ROUTES:
+Requests should be made to localhost on port 3000, with the listed JSON arguments in the request ```diff +body ```
+
+ADD NEW TRANSACTION
+  type: POST
+  endpoint: /transactions
+  body-argument: {
+    payer: <string>
+    points: <integer>
+    timestamp: <UTC date>
+  }
+
+SPEND/CONSUME POINTS
+  type: PUT
+  endpoint: /spend
+  body-argument: {
+    points: <integer>
+  }
+
+GET ALL POINT BALANCES BY PAYER:
+  type: GET
+  endpoint: /balances
+
+
+HOW-TO
+1. To start the server, from the root folder, run `npm run start`.
+
+2. Send HTTP requests from your local computer as outlined above.
+
+
+DATA LOGIC:
+
+All data is being stored in memory in the model.js file.
+
+```diff +transactions``` are being stored in an array, and are entered in chronologically at insertion.
+
+All transactions are stored here including any transactions that have a negative point value.
+
+Each element contains the payer, points, and a timestamp.
+
+Transactions are never altered or removed once entered for reporting integrity.
+
+
+```diff +accumulatedPoints``` is an object that stores the total number of points ever accumulated separated by payer.
+
+Each key is the name of a single payer, with a value equal to the points ever accumulated for that payer
+
+```diff +removedPoints``` has the same structure as accumulatedPoints, but keeps track of all points ever removed or used for each payer.
+
+These values include points removed from a transaction with a negative point value, or points that are used by the user.
+
+These values are used to know how many points should be 'ignored' in two ways:
+  1. When calculating the total balance of points per each payer
+  2. When a user consumes points. the amount of 'removed points' per payer will be ignored as the transactions are evaluated chronologically by looping across the transactions array.
+
+
+
--- server/model.js
+++ server/model.js
@@ -15,7 +15,7 @@ async function addTransaction(transaction) {
   //insert into the transaction array in order
   logTransaction(transaction);
   accumulatedPoints[transaction.payer] += transaction.points; //updates payer
-  return 'Success'; //TODO: maybe change this
+  return 'Success';
 }
 
 
@@ -32,25 +32,19 @@ async function getBalances() {
 
 async function spendPoints(points) {
   //determine total points
-  console.log(points);
   let totalPoints = 0;
   const balances = await getBalances();
   for (payer in balances) {
     totalPoints += balances[payer];
   }
-  //if total points < requested points send an error
   if (totalPoints < points) {
     throw new RangeError('Not enough points to fulfill this request');
   }
-  //create an array to hold the response objects
   let response = [];
-  //create a temp object to hold all payers/points to be consumed
   let consumedPoints = {};
-  //make a copy of the removed points object
   let removed = { ...removedPoints };
+  //helper func to store consumed points data for individual transactions
   const addPayerPoints = (payer, transPoints) => {
-    console.log('payer', payer);
-    console.log('transPoints', transPoints);
     let pointsToAdd = points - transPoints >= 0 ? transPoints : points;
     if (!consumedPoints[payer]) {
       consumedPoints[payer] = 0
@@ -58,31 +52,17 @@ async function spendPoints(points) {
     consumedPoints[payer] += pointsToAdd;
     points -= pointsToAdd;
   }
-  //loop through transactions
+
   let i = 0;
-  while (points > 0) {
-    console.log('points ', points);
-    console.log('transaction, ', transactions[i]);
-    console.log('removed ', removed);
-    //if the removed points for that payer is 0
-    //add the payer to the holder object if it's not there
-    //increment their points to be used
-    //decrement totalpoints to be consumed
+  while (points > 0) { //this loop compares the current transaction points against removed points for that payer, in order of timestamp
+    //skip over transactions that were negative point transactions
     if (transactions[i].points < 0) {
       i++;
       continue;
     } else if (removed[transactions[i].payer] === 0) {
       addPayerPoints(transactions[i].payer, transactions[i].points);
-      //else if removed points >= current transaction's points
-      //decrement removed points for that payer by current transaction's points
     } else if (removed[transactions[i].payer] >= transactions[i].points) {
       removed[transactions[i].payer] -= transactions[i].points;
-
-      //else
-      //add the payer to the holder object if it's not there
-      //increment their points to be used by transaction points - removedpoints remaining
-      //decrement totalpoints to be consumed by transaction points - removedpoints remaining
-      //set removed points for that payer to 0;
     } else {
       addPayerPoints(transactions[i].payer, transactions[i].points - removed[transactions[i].payer]) //only passes in points remaining after removed points are accounted for
       removed[transactions[i].payer] = 0;
@@ -92,10 +72,7 @@ async function spendPoints(points) {
   for (payer in consumedPoints) {
     response.push({ "payer": payer, "points": -consumedPoints[payer] })
     removedPoints[payer] += consumedPoints[payer];
-    //TODO: add these amounts to actual removed values
   }
-  //for each key in the temp object, push it's values into the return array
-  console.log('response ', response);
   return response;
 }
 
